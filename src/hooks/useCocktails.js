8f7b45b62dad4b349b3da1cd7ce4f67f19eed720
hivictording <jianboding@outlook.com>
2020-09-29 00:01:09 -0400

    set loading state

--- src/hooks/useCocktails.js
+++ src/hooks/useCocktails.js
@@ -6,10 +6,11 @@ const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
 
 export const useCocktails = (search) => {
   const [cocktails, setCocktails] = useState([]);
-  const [loading, setLoading] = useState("true");
+  const [loading, setLoading] = useState();
   const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       if (search) {
         let response = await axios.get(`${baseURL}${search}`);
@@ -20,6 +21,7 @@ export const useCocktails = (search) => {
     };
 
     fetchData();
+    setLoading(false);
   }, [search]);
 
   return [cocktails, loading, error];
